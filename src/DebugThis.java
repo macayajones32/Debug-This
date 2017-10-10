be63d8c64af4dd304d3987978855b3845c9c5a38
Macaya Jones <macaya.jones32@gmail.com>
2017-10-10 16:54:04 -0400

    first commit

---
+++ src/DebugThis.java
@@ -0,0 +1,12 @@
+public class DebugThis {
+    public static void main(String[] args) {
+         double x, y;
+         /* changed int to double */
+
+        x = 3.1415;
+        y = 3.64;
+
+        System.out.println("pi is approximately " + x);
+        System.out.println("My GPA was " + y);
+    }
+}
